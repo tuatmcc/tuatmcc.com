bace77169a81f50f57e28e872eec183f14166b61
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-04-15 22:08:17 +0900

    remove: unused drawer

--- src/app/_components/Navbar/index.tsx
+++ src/app/_components/Navbar/index.tsx
@@ -4,8 +4,6 @@ import classnames from 'classnames';
 import NextLink from 'next/link';
 import { type FC, useCallback, useState } from 'react';
 
-import { Drawer } from './Drawer';
-import { MenuButton } from './MenuButton';
 import styles from './styles.module.css';
 
 import WordmarkLogo from '/public/icons/wordmark-logo.svg';
@@ -22,17 +20,6 @@ export const Navbar: FC<NavbarProps> = ({
   color = 'mcc',
   noBrand = false,
 }) => {
-  // const [isOpen, setIsOpen] = useState<boolean>(false);
-  // const setNavDrawerState = useCallback((bool: boolean) => {
-  //   if (document.startViewTransition) {
-  //     document.startViewTransition(() => {
-  //       setIsOpen(bool);
-  //     });
-  //   } else {
-  //     setIsOpen(bool);
-  //   }
-  // }, []);
-
   return (
     <>
       <nav className={styles.nav}>
@@ -51,15 +38,6 @@ export const Navbar: FC<NavbarProps> = ({
             </NextLink>
           )}
         </div>
-        {/* <Drawer */}
-        {/*   isOpen={isOpen} */}
-        {/*   onClickOutside={() => setNavDrawerState(false)} */}
-        {/* /> */}
-        {/* <MenuButton */}
-        {/*   isExpanded={isOpen} */}
-        {/*   onClick={() => setNavDrawerState(!isOpen)} */}
-        {/*   color={isOpen ? 'white' : color.replace('mcc', '#06c')} */}
-        {/* /> */}
       </nav>
     </>
   );
